29af7a3654c7bcb5e46742f3c260256b009485c0
Rudi Farkas <rudi.farkas@gmail.com>
2022-04-26 00:22:11 +0200

    update from use-decorators

---
+++ src/my-element.js
@@ -0,0 +1,42 @@
+import { html, css, LitElement } from "lit";
+import { property, customElement } from "lit/decorators.js";
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement("my-element")
+export class MyElement extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        border: solid 1px gray;
+        padding: 16px;
+        max-width: 800px;
+        background-color: #d6f7f093;
+      }
+    `;
+  }
+
+  @property({ type: String })
+  name = "Wide World";
+
+  @property({ type: Number })
+  count = 0;
+
+  render() {
+    return html`
+      <h1>Hello, ${this.name}!</h1>
+      <button @click=${this._onClick} part="button">
+        Click Count: ${this.count}
+      </button>
+      <slot></slot>
+    `;
+  }
+
+  _onClick() {
+    this.count++;
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    babel({
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: [
+          [
+            "@babel/plugin-proposal-decorators",
+            { decoratorsBeforeExport: true },
+          ],
+          ["@babel/plugin-proposal-class-properties", { loose: true }],
+        ],
+      },
+    }),
+  ],
   build: {
     // lib: {
     //   entry: 'src/my-element.js',
@@ -10,5 +26,8 @@ export default defineConfig({
     // rollupOptions: {
     //   external: /^lit/
     // }
+  },
+  server: {
+    open: "/index.html",
   },
 });
